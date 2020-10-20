04309161c21c4458a5b69a23567d19bbad845ef8
shubham 7822 <shubhammansute7822@gmail.com>
2020-10-21 03:35:03 +0530

    Updates

---
+++ precache-manifest.e6d02d1f00bd472225d6c26c5f35ae8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d7546622c4431bdb43375a2adf4a9b",
+    "url": "/amazon-clone-in-react/index.html"
+  },
+  {
+    "revision": "d17a4722ff6a9d780e04",
+    "url": "/amazon-clone-in-react/static/css/main.bd39fe40.chunk.css"
+  },
+  {
+    "revision": "b6735546ce5d1b5bfb00",
+    "url": "/amazon-clone-in-react/static/js/2.f930b61d.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/amazon-clone-in-react/static/js/2.f930b61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17a4722ff6a9d780e04",
+    "url": "/amazon-clone-in-react/static/js/main.755b8407.chunk.js"
+  },
+  {
+    "revision": "0c381c4d257c1e5e569a",
+    "url": "/amazon-clone-in-react/static/js/runtime-main.f8ca0bf5.js"
+  }
+]);
